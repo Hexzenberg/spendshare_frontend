c4521763cc241b770f285148d1eebe0e5dbb9627
Nikhil Kumar Chaudhary <nkc150802@gmail.com>
2025-03-15 21:12:06 +0530

    Update PredictForm.js

--- src/Components/PredictForm.js
+++ src/Components/PredictForm.js
@@ -27,7 +27,7 @@ const PredictForm = () => {
         setPrediction(null);
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/predict", {
+            const response = await fetch("https://spendshare-api.onrender.com", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
